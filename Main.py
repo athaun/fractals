36ea7761a032847506a455d1bb9b65b673c64aa1
Ryan Knobel <r.knobel27@yahoo.com>
2024-09-11 15:23:18 -0500

    Added gui

--- Main.py
+++ Main.py
@@ -1,4 +1,151 @@
 import Fractal_Logic as fl
+import tkinter as tk
+
+# Tile size and locations
+TILE_SIZE = 30
+
+# Useful functions
+def get_tag(x, y):
+    return str(x) + ',' + str(y)
+
+def get_xy(x, y):
+    return [60 * round(x / 60), 60 * round(y / 60)]
+
+# Main
+class main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        self.tile_positions=dict([])
+        self.origin_tile = None
+
+        for F in (draw_seed, choose_origin):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(draw_seed)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def update_frame(self, cont):
+        frame = self.frames[cont]
+        frame.update(self)
+        self.frames[cont] = frame
+
+    def finish(self):
+        if self.origin_tile != None:
+            print('Done!')
+
+class draw_seed(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+
+        label = tk.Label(self, text = "Left click to place tile. Right click to delete. Done to finish.")
+        label.pack()
+
+        # Canvas for drawing
+        canvas = tk.Canvas(self, bg='lightgray')
+        canvas.pack(anchor='nw', fill='both', expand=1)
+
+        def add_tile(event):
+            global lasx, lasy
+            lasx, lasy = event.x, event.y
+
+            # Round
+            x, y = get_xy(lasx, lasy)
+
+            if get_tag(x, y) not in controller.tile_positions:
+                tile = canvas.create_rectangle(x - TILE_SIZE, y - TILE_SIZE, x + TILE_SIZE, y + TILE_SIZE, fill='white')
+                controller.tile_positions[get_tag(x,y)] = (tile, x, y, 0)
+
+
+        def remove_tile(event):
+            global lasx, lasy
+            lasx, lasy = event.x, event.y
+
+            # Round
+            x, y = get_xy(lasx, lasy)
+
+            if get_tag(x,y) in controller.tile_positions:
+                canvas.delete(controller.tile_positions[get_tag(x, y)][0])
+                del controller.tile_positions[get_tag(x, y)]
+
+        canvas.bind("<Button-1>", add_tile)
+        canvas.bind("<Button-3>", remove_tile)
+
+        def display_choose_origin():
+            controller.update_frame(choose_origin)
+            controller.show_frame(choose_origin)
+
+        button = tk.Button(self, text="Done", command=display_choose_origin)
+        button.pack()
+
+class choose_origin(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+
+    def update(self, controller):
+        label = tk.Label(self, text = "Left click to select origin tile. Done to finish.")
+        label.pack()
+
+        # Redraw canvas
+        canvas = tk.Canvas(self, bg='lightgray')
+        canvas.pack(anchor='nw', fill='both', expand=1)
+
+        for cord in controller.tile_positions:
+            [x,y] = cord.split(',')
+            x, y = int(x), int(y)
+            canvas.create_rectangle(x - TILE_SIZE, y - TILE_SIZE, x + TILE_SIZE, y + TILE_SIZE, fill='white')
+
+        def choosing_tile(event):
+            global lasx, lasy
+            lasx, lasy = event.x, event.y
+
+            # Round
+            x, y = get_xy(lasx, lasy)
+            
+            if get_tag(x,y) in controller.tile_positions: 
+                for cord in controller.tile_positions:
+                    [w,z] = cord.split(',')
+                    w, z = int(w), int(z)
+                    if controller.tile_positions[get_tag(w,z)][3] == 1:
+                        canvas.delete(controller.tile_positions[get_tag(w, z)][0])
+                        del controller.tile_positions[get_tag(w, z)]
+
+                        tile = canvas.create_rectangle(w - TILE_SIZE, z - TILE_SIZE, w + TILE_SIZE, z + TILE_SIZE, fill='white')
+                        controller.tile_positions[get_tag(w,z)] = (tile, w, z, 0)
+
+                        break
+                
+                canvas.delete(controller.tile_positions[get_tag(x, y)])
+                del controller.tile_positions[get_tag(x, y)]
+
+                tile = canvas.create_rectangle(x - TILE_SIZE, y - TILE_SIZE, x + TILE_SIZE, y + TILE_SIZE, fill='black')
+                controller.tile_positions[get_tag(x,y)] = (tile, x, y, 1)
+
+                controller.origin_tile = tile
+        
+        canvas.bind("<Button-1>", choosing_tile)
+
+        button = tk.Button(self, text="Done",
+                            command=lambda: controller.finish())
+        button.pack()
+
+app = main()
+app.mainloop()
+
 
 # # Sierpinski Triangle seed
 # seed_tile = fl.Tile(None, ["E"])
@@ -18,9 +165,6 @@ import Fractal_Logic as fl
 # t2.key_tile_N, t2.key_tile_E, t2.key_tile_W, t2.key_tile_S = None, ["S"], ["S"], ["S"]
 
 # t2.terminal = True
-
-
-
 
 # # Horseshoe
 # seed_tile = fl.Tile(None, ['S'])
@@ -128,30 +272,30 @@ import Fractal_Logic as fl
 
 
 
-# Sierpinski triangle ("Schweller" way)
-seed_tile = fl.Tile(None, ['N', 'E'])
-t1 = fl.Tile(['S'], None)
-t2 = fl.Tile(['W'], None)
+# # Sierpinski triangle ("Schweller" way)
+# seed_tile = fl.Tile(None, ['N', 'E'])
+# t1 = fl.Tile(['S'], None)
+# t2 = fl.Tile(['W'], None)
 
-seed_tile.tile_to_N, t1.tile_to_S = t1, seed_tile
-seed_tile.tile_to_E, t2.tile_to_W = t2, seed_tile
+# seed_tile.tile_to_N, t1.tile_to_S = t1, seed_tile
+# seed_tile.tile_to_E, t2.tile_to_W = t2, seed_tile
 
-seed_tile.N, t1.S = 'N', 'N'
-seed_tile.E, t2.W = 'N', 'N'
+# seed_tile.N, t1.S = 'N', 'N'
+# seed_tile.E, t2.W = 'N', 'N'
 
-seed_tile.key_tile_N, seed_tile.key_tile_E, seed_tile.key_tile_W, seed_tile.key_tile_S = ['N'], ['E'], None, None
-t1.key_tile_N, t1.key_tile_E, t1.key_tile_W, t1.key_tile_S = None, ['S'], ['S'], ['S']
-t2.key_tile_N, t2.key_tile_E, t2.key_tile_W, t2.key_tile_S = ['W'], None, ['W'], ['W']
+# seed_tile.key_tile_N, seed_tile.key_tile_E, seed_tile.key_tile_W, seed_tile.key_tile_S = ['N'], ['E'], None, None
+# t1.key_tile_N, t1.key_tile_E, t1.key_tile_W, t1.key_tile_S = None, ['S'], ['S'], ['S']
+# t2.key_tile_N, t2.key_tile_E, t2.key_tile_W, t2.key_tile_S = ['W'], None, ['W'], ['W']
 
-t1.terminal, t2.terminal = True, True
-seed_tile.terminal = False
+# t1.terminal, t2.terminal = True, True
+# seed_tile.terminal = False
 
 
-# Doesnt change
-seed_tile.original_seed = True
+# # Doesnt change
+# seed_tile.original_seed = True
 
 # Plot --------------------------------------------------------------------------------------------------------
-result = fl.run_simulation(seed_tile, 1)
+# result = fl.run_simulation(seed_tile, 1)
 
-# Plot the result onto graph
-fl.plot_graph(seed_tile)
+# # Plot the result onto graph
+# fl.plot_graph(seed_tile)
